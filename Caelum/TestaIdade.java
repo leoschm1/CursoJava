568af21c605ff49fe7576ad7644aab059a3f7c15
Leonardo Schmucker <leonardo.schmucker@linx.com.br>
2017-03-15 14:12:08 -0300

    corrigindo pastas caelum e curso youtube

---
+++ Caelum/TestaIdade.java
@@ -0,0 +1,15 @@
+class TestaIdade {
+
+    public static void main(String[] args) {
+       
+        int idade = 25;
+        System.out.println("A idade atual é " + idade);
+
+       
+        int idadeNoAnoQueVem;
+        idadeNoAnoQueVem = idade + 1;
+
+     
+        System.out.println("A idade ano que vem será " + idadeNoAnoQueVem);
+    }
+}
