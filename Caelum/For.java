ba4b394ccde88ba4da31b5400bc1ef45218615cb
Leonardo Schmucker <leonardo.schmucker@linx.com.br>
2017-03-15 18:47:13 -0300

    ex caelum

---
+++ Caelum/For.java
@@ -0,0 +1,28 @@
+
+
+class Exercicio1 {
+    public static void main(String[] args) {
+        //1
+        for (i = 0 ; i <= 100 ; i++){
+        	System.out.println(i);
+        }
+        //2
+    	int soma;
+    	for (i = 0; i <=1000; i++){
+    		soma = soma + i;
+    	}
+    	System.out.println("A soma é :" soma);
+
+    	//3
+    	for(i=0 ; i < 100 ; i++){
+    		if(i % 3 == 0){
+    			System.out.println(i + "é multiplo de 3");
+    		}
+    	}
+
+    	//4
+
+    }
+}
+
+
