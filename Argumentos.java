f0fd0d70295f1c4dded1cd7b19715c42aa62fc9c
Leonardo Schmucker <leonardo.schmucker@linx.com.br>
2017-03-15 12:30:46 -0300

    variáveis primitivas

--- Argumentos.java
+++ Argumentos.java
@@ -2,5 +2,6 @@ class Argumentos {
 	public static void main (String[] args){
 
 		System.out.println("Você digitou :" + args[0]);
+		//sistema trará "Você digitou + Argumentos que vocÊ passou ao rodar"
 	}
 }
---
+++ VarPrimitivos.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+
+
+	public class void main (String[] args){
+	
+		int idade;
+		String nome = "Leonardo"
+		String nomeCachorro = "Gaveta"
+
+		idade = 25;
+
+		System.out.println("Meu nome é " + nome + "tenho " + idade + "anos");
+
+	}
+
+}
